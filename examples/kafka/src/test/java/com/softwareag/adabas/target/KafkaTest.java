7ef50e34c17ab965a9b07010c389c2c0d8c85731
Matthias Gerth <ger@softwareag.com>
2023-09-22 16:20:01 +0200

    upgrade to gradle 8.3

--- examples/kafka/src/test/java/com/softwareag/adabas/target/KafkaTest.java
+++ examples/kafka/src/test/java/com/softwareag/adabas/target/KafkaTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Software AG, Darmstadt, Germany and/or Software AG USA 
+ * Copyright (c) 2021-2023 Software AG, Darmstadt, Germany and/or Software AG USA 
  * Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates
  * and/or their licensors.
  * Use, reproduction, transfer, publication or disclosure is prohibited except
